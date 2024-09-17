b444f7e9d3566f8dbe6240765152096becdd230d
addpeoplemain <167210268+addpeoplemain@users.noreply.github.com>
2024-09-17 12:45:26 +0100

    Update onboarding_dashboard.py

--- onboarding_dashboard.py
+++ onboarding_dashboard.py
@@ -8,3 +8,28 @@ import tldextract
 from bs4 import BeautifulSoup
 
 st.title("Add People App Centre")
+
+st.markdown("""
+<style>
+    [data-testid=stSidebar] {
+        background-color:#F7F5F2 ;
+        color: #173340;
+        
+    }
+  
+
+</style>
+""", unsafe_allow_html=True)
+
+with st.sidebar:
+    st.image("logo.png")
+    st.write("""
+                 
+         **Onboarding Application 1.0**
+         
+         Onboarding Application 1.0
+                 
+        
+
+            """
+         )
