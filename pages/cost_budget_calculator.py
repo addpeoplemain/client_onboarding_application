9d6ada807a74bebe9cbe69b58b00c3e92e7e90ad
addpeoplemain <167210268+addpeoplemain@users.noreply.github.com>
2024-09-17 14:52:14 +0100

    Update cost_budget_calculator.py

--- pages/cost_budget_calculator.py
+++ pages/cost_budget_calculator.py
@@ -93,8 +93,8 @@ def cpc_month_editor():
 cpc_month_editor()
 
 cpc_month__edited_df = st.session_state["cpc_month_df"]
-cpc = cpc_month__edited_df['Num'].iloc[0]
-month_cost = cpc_month__edited_df['Num'].iloc[1]
+cpc = round(cpc_month__edited_df['Num'].iloc[0], 2)
+month_cost = round(cpc_month__edited_df['Num'].iloc[1], 2)
 conversion_cpc = spend_per_conversion_with_condition(cpc, month_cost, cta_list)
 
 st.info(f"With a monthly budget of £{month_cost} and a cost per click of £{cpc}. You are expected to receive {conversion_cpc[0]} conversions with a cost per conversion of {conversion_cpc[1]}")
