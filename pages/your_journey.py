d05169caf29b81cb1dc0cf38b83ebb912dfecfd8
addpeoplemain <167210268+addpeoplemain@users.noreply.github.com>
2024-09-18 11:31:59 +0100

    Update your_journey.py

--- pages/your_journey.py
+++ pages/your_journey.py
@@ -1,1 +1,33 @@
+import streamlit as st
+from streamlit_tags import st_tags
+
+
+st.title("Add People: Your Journey With Add People")
+
+st.markdown("""
+<style>
+    [data-testid=stSidebar] {
+        background-color:#F7F5F2 ;
+        color: #173340;
+        
+    }
+  
+
+</style>
+""", unsafe_allow_html=True)
+
+with st.sidebar:
+    st.image("logo.png")
+    st.write("""
+                 
+         **Keyword Competitor Analysis 1.0**
+         
+         Onboarding Application 1.0
+                 
+        
+
+            """
+         )
+
+
 
