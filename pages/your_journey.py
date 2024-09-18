fdf903132f6da7b35346cbb52ba67c4c60fce649
addpeoplemain <167210268+addpeoplemain@users.noreply.github.com>
2024-09-18 12:14:28 +0100

    Update your_journey.py

--- pages/your_journey.py
+++ pages/your_journey.py
@@ -2,15 +2,6 @@ import streamlit as st
 from streamlit_tags import st_tags
 
 
-st.title("Add People")
-st.header("Your Journey With Add People")
-
-st.subheader("Sales Call")
-st.subheader("Onboarding")
-st.write("Day 2-3 : Welcome Call Booking")
-st.write("Day 2-3 : Welcome Call Delivery")
-st.write("Day 2-3 : Welcome Call Delivery")
-
 st.markdown("""
 <style>
     [data-testid=stSidebar] {
@@ -26,15 +17,24 @@ st.markdown("""
 with st.sidebar:
     st.image("logo.png")
     st.write("""
-                 
-         **Keyword Competitor Analysis 1.0**
          
          Onboarding Application 1.0
-                 
-        
 
             """
          )
+
+
+st.title("Add People")
+st.header("Your Journey With Add People")
+
+st.header("Onboarding")
+st.subheader("Day 2-3 : Welcome Call Booking")
+st.subheader("Day 4-8 : Welcome Call Delivery")
+st.subheader("Day 5-18 : Processes")
+st.subheader("Day 16-19 : Launch Call")
+st.subheader("Day 30-40 : Review Call")
+st.subheader("Day 40+ : Comms")
+st.subheader("Day 40+ : Tech")
 
 
 
