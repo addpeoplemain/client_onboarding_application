e6787df6bb2e1567f142e8fec1364da5a12107e3
addpeoplemain <167210268+addpeoplemain@users.noreply.github.com>
2024-09-18 12:08:12 +0100

    Update onboarding_dashboard.py

--- onboarding_dashboard.py
+++ onboarding_dashboard.py
@@ -16,6 +16,16 @@ st.markdown("""
         color: #173340;
         
     }
+
+    button {
+    background-color: blue; /* Green */
+    color: black;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+}
   
 
 </style>
